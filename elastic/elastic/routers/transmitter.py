7beb0ecb2492afc7b2b4826fb94a37f08e791ee7
Arc1el <khm970514@gmail.com>
2023-02-09 14:07:38 +0900

    dotenv, add annotation, modify exception logic

--- elastic/elastic/routers/transmitter.py
+++ elastic/elastic/routers/transmitter.py
@@ -1,26 +1,37 @@
-from typing import Optional
-from fastapi import APIRouter, FastAPI, Body
+# using fastapi router
+from nis import cat
+from fastapi import APIRouter
 router = APIRouter()
-import os
-import elasticsearch
+
+# define json data format
 from pydantic import BaseModel
+from typing import Optional
+import json
 
+# using elasticsearch database (elasic cloud)
+import elasticsearch
+
+# load dotenv data
+import os
 from dotenv import load_dotenv
-load_dotenv()
 
+# set values
+load_dotenv()
 CLOUD_ID = os.getenv("CLOUD_ID")
-print(CLOUD_ID)
 CLOUD_USER = os.getenv("CLOUD_USER")
 CLOUD_PSWD = os.getenv("CLOUD_PSWD")
 
+# attach to the elasticserach database
 es = elasticsearch.Elasticsearch(
-    cloud_id="crawl:dXMtZWFzdC0yLmF3cy5lbGFzdGljLWNsb3VkLmNvbTo0NDMkNWFlNjQyNWRjNGJiNDRjNThlYjExMTdkMzVlNmQ0NGQkMjJjYjEwZTA2OTEzNDYyNzg1MTY4N2MzZGM3ZGExNTM=",
-    http_auth=("elastic", "qFEupQHbmLZYFGT6jG8ZvRTm"),
+    cloud_id=CLOUD_ID,
+    http_auth=(CLOUD_USER, CLOUD_PSWD),
 )
 
+# define the json data format
 class Item(BaseModel):
     sitename : Optional[str]
     url : Optional[str]
+    title : Optional[str]
     collectiondate : Optional[str]
     startdate : Optional[str]
     enddate : Optional[str]
@@ -39,12 +50,39 @@ class Item(BaseModel):
     description : Optional[str]
     stacks : Optional[str]
     
-
-@router.post("/post")
-async def post(data : dict):
+""" Example Request Body
+{
+    "0": {
+        "sitename": "SEEK",
+        "url": "https:\/\/www.seek.com.au\/job\/60157989?type=promoted",
+        "title" : "Receptionists (Administration & Office Support)",
+        "collectiondate": "1675836363",
+        "startdate": "1675836363",
+        "enddate": "",
+        "companyname": "The Trustee for the Dr Maree Wilkins practice trust",
+        "location": "Gladesville, Sydney NSW",
+        "recruitfield": "",
+        "recruittype": "",
+        "recruitclassification": "",
+        "personnel": "",
+        "salary": "",
+        "position": "Receptionists (Administration & Office Support)",
+        "task": "",
+        "qualifications": "",
+        "prefer": "",
+        "welfare": "",
+        "description": "Which of the following statements best describes your right to work in Australia?"
+    }
+}
+"""
     
-    for key, value in data.items():
-        res = es.index(index="jobs", id=key, body=value)
-        
-    print(res)
-    return res
+# elastic router. Send json data to elastic cloud
+@router.post("/elastic")
+async def elastic(data : dict):
+    try:
+        for key, value in data.items():
+            res = es.index(index="crawl_database", id=key, body=value)
+            print(res)
+        return res
+    except Exception as e:
+        return e
