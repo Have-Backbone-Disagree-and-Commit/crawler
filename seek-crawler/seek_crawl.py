774b7020764bb3f193be42174a08cc30f3ebb691
= <kimhuiji@gimhuijiui-MacBookAir.local>
2023-02-17 21:40:54 +0900

    test

--- seek-crawler/seek_crawl.py
+++ seek-crawler/seek_crawl.py
@@ -11,7 +11,6 @@ import numpy
 import pandas
 from bs4 import BeautifulSoup
 from slack_sdk import WebClient
-from slack_sdk.errors import SlackApiError
 import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
